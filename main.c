d556a34789cb1866a69d46f8cd46e7a222837abc
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-08-13 13:46:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,29 +5,37 @@
 #include <winuser.h>
 #include <processthreadsapi.h>
 #include <string.h>
-
+#pragma comment(lib, "winmm.lib")
 
 
 // run :
 // a.exe "filepath"
-
+DWORD thread_ID;  
 
 DWORD WINAPI PlayMusic(LPVOID data){
     char * audiofile = data;
     
     char command[MAX_PATH];
-    
+    MCIERROR err;
    
     printf("Now playing %s\n",audiofile);
     sprintf(command,"open \"%s\" type mpegvideo alias audiofile", audiofile); // constructing the command string
     
     mciSendString(command, NULL, 0, NULL ); //opening the media file
-    mciSendString("play audiofile", NULL,0,NULL);//playing the file by its alias name
+   
+    err = mciSendString("play audiofile", NULL,0,NULL);//playing the file by its alias name
+    if (err)
+    {
+       puts("Error playing file");
+        
+        ExitProcess(-1);
+    }
     
     MSG lpMsg; //Inter-Threads message handling
     while (GetMessageA(&lpMsg, 0,0,0)){ //waiting for incoming messages sent to the thread using its unique thread id
         switch (lpMsg.message) //traiting the messages
         {
+     
         case WM_APP + 1:
             printf("Pausing...\n");
             mciSendString("pause audiofile", NULL,0,NULL);
@@ -52,11 +60,10 @@ DWORD WINAPI PlayMusic(LPVOID data){
 int main(int argcount, char* argvalues[]){
     
     if (argcount > 1 ){
-        DWORD thread_ID;       
+             
         HANDLE threadHandle = CreateThread(NULL, 0,PlayMusic, argvalues[1],0, &thread_ID); //create the thread that will play the music
         Sleep(500); //sleeping half a second to prevent thread racing
         if (threadHandle){ //if the thread is created the function will retun its handle
-            printf("Thread created... playing music...\n");
             char c;
             int test = 1;
             while (test){// waiting for user input commands p for pause r for resume and c for closing
